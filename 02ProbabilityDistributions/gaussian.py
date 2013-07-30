43c93fe0190f1a8d65d696bdbe6f668524a99261
evilncrazy <user2@server.fake>
2013-07-30 13:09:31 +1000

    Add plot with eigenvalues of covariance matrix

--- 02ProbabilityDistributions/gaussian.py
+++ 02ProbabilityDistributions/gaussian.py
@@ -26,5 +26,36 @@ def plotScatterDiagonal():
    plt.legend()
    plt.show()
 
+def plotScatterEigen():
+   """
+   Plots a scatter plot of a bivariate Gaussian distribution, along with the
+   eigenvectors and eigenvalues of the covariance matrix, which represent the
+   axis of the elliptical contour when the probability density is equal to
+   exp(-0.5)
+   """
+   # Set the mean to zero
+   mean = [0, 0]
+
+   # Use a diagonal covariance
+   cov = [[100, 50], [30, 100]]
+
+   # Plot a scatter of the distribution
+   x, y = np.random.multivariate_normal(mean, cov, 1000).T
+
+   # Plot the points
+   plt.scatter(x, y, alpha=0.2)
+
+   # Find the eigenvectors and eigenvalues of the covariance matrix
+   w, v = np.linalg.eig(cov)
+   x0, y0 = v.item(0, 0) * np.sqrt(w[0]), v.item(1, 0) * np.sqrt(w[0])
+   x1, y1 = v.item(0, 1) * np.sqrt(w[1]), v.item(1, 1) * np.sqrt(w[1])
+
+   # Plot the eigenvectors and eigenvalues
+   plt.plot([-x0, x0], [-y0, y0], linewidth=3)
+   plt.plot([-x1, x1], [-y1, y1], linewidth=3)
+
+   plt.show()
+
 if __name__ == "__main__":
+   plotScatterEigen()
    plotScatterDiagonal()
